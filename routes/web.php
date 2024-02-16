9d2df097c6e006183671daec5244891bc1ad5544
JackyLinux <jackkyyyyy25@gmail.com>
2024-02-16 20:18:21 +0800

    0

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
-    ddd($posts);
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
         ]);
 });
 
